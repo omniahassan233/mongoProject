202c79fedc09f7ee6d6cf792398dbcfef89db20f
Omnia <omnia.hassan.inovaeg@gmail.com>
2024-01-01 18:11:50 +0200

    basic strucyure

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import cors from "cors";
 import express from "express";
-
 import { connection } from "./data_base/connection.js";
 
 
 const port = 55555;
 const app = express();
 
-
 app.use(cors());
-
+app.use(express.json());
 
 connection();
-
-
-
-app.get("/" , (req,res)=>{
-    res.json({message:"hello"})
-})
-
 
 app.listen(port , () => {
     console.log("App is Up and running on port: "+port);
