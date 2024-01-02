7df513b617aa647411b9d5785475909cda34f3ac
Omnia <omnia.hassan.inovaeg@gmail.com>
2024-01-02 13:10:40 +0200

    user cruds

--- data_base/connection.js
+++ data_base/connection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export const connection = () =>{
-    mongoose.set("strictQuery",true);
+    // mongoose.set("strictQuery",true);
     mongoose.connect("mongodb://localhost:27017/route")
     .then(()=>{
         console.log("Connected to Mongo DB successfully");
     })
     .catch((err)=>{
-        console.log("Error Connecting to Mongo DB");
+        console.log("Error Connecting to Mongo DB",err);
     });
 }
--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,18 +1,29 @@
-const getAllUsers = (req,res) =>{
-    res.json({message:"getAllUsers"})
+import userModel from "../../../../data_base/models/user.model.js"
+
+const getAllUsers = async(req,res) =>{
+    const users = await userModel.find().select("-email -age"); //will return every thing except for email nor age^^
+    // const users = await userModel.findById("6592f5771af303c4e41a6e3c",{age:0,email:0}); //will return every thing except for email nor age^^
+    res.json({message:"getAllUsers",users})
 }
 
-const addUser = (req,res) =>{
-    res.json({message:"addUser"})
+const addUser = async(req,res) =>{
+    let {name,age,email,password} =req.body;
+    const addedUser = await userModel.insertMany({name,age,email,password});
+    res.json({message:"addUser",addedUser})
 }
 
 
-const updateUser = (req,res) =>{
-    res.json({message:"updateUser"})
+const updateUser = async(req,res) =>{
+    let {_id,name} =req.body;
+    // const updatedUser = await userModel.findByIdAndUpdate(_id, {name}); //this will update and return old object
+    const updatedUser = await userModel.findByIdAndUpdate(_id, {name},{new:true}); 
+    res.json({message:"updateUser",updatedUser})
 }
 
-const deleteUser = (req,res) =>{
-    res.json({message:"deleteUser"})
+const deleteUser = async(req,res) =>{
+    let {_id} =req.body;
+    const deletedUser = await userModel.deleteOne({_id});
+    res.json({message:"deleteUser",deletedUser});
 }
 
 
